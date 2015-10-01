735376ec294d631cc4ea5a30bacee54cb07f167f
Dinusha <dinusha.weerasekara@pearson.com>
2015-10-01 19:18:14 +0530

    befor kpi report

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/home', function(req, res) {
+  res.render('index');
 });
+router.get('/', function(req, res) {
+  res.render('login');
+});
+
+router.get('/reg', function(req, res) {
+  res.render('reg');
+});
+
+router.get('/release', function(req, res) {
+  res.render('release');
+});
+router.get('/scale', function(req, res) {
+  res.render('scale');
+});
+
+
+
 
 module.exports = router;
